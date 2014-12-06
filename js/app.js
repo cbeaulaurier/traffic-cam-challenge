3b29e692fe83a53ce692f825ccde92d22989a965
Cassandra Beaulaurier <cassbeau@uw.edu>
2014-12-05 19:32:05 -0800

    added functionality, changed font and error message css

--- js/app.js
+++ js/app.js
@@ -11,3 +11,72 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function () {
+    function createMap(center, zoom) {
+        var mapElem = document.getElementById('map');
+        var map = new google.maps.Map(mapElem, {
+            center: center,
+            zoom: zoom
+        });
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+            .done(function(data) {
+                data.forEach(function(camera) {
+                    var marker = new google.maps.Marker({
+                        position: {
+                            lat: Number(camera.location.latitude),
+                            lng: Number(camera.location.longitude)
+                        },
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        map.panTo(marker.getPosition());
+                        var html = '<h2>' + camera.cameralabel + '</h2>';
+                        html += '<img src="' + camera.imageurl.url + '"/>';
+                        infoWindow.setContent(html);
+                        infoWindow.open(map, this);
+                    });
+
+                    $('#search').bind('search keyup', function() {
+                        if (camera.cameralabel.toLowerCase().indexOf(this.value.toLowerCase()) < 0) {
+                            marker.setMap(null);
+                        } else {
+                            marker.setMap(map);
+                        }
+                    });
+                });
+                $('.error-message').css('display', 'none');
+            }) // success case
+            .fail(function(error) {
+                $('.error-message').css('display', 'block');
+            })
+    } // createMap()
+
+    var defaultCoords = {
+        lat: 47.655,
+        lng: -122.3080
+    };
+
+    function onGeoSuccess(position) {
+        var center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        createMap(center, 12);
+    } // onGeoSuccess()
+
+    function onGeoError(err) {
+        console.log(err);
+    } // onGeoError()
+
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, {
+            enableHighAccuracy: true            // enables GPS
+        });
+    } else {
+        createMap(defaultCoords, 12);
+    }
+});
